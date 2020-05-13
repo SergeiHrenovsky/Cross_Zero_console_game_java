37e608982539200cd8ed13161efbb66d5047b67d
sergei <sergeihrenovsky@yandex.by>
2020-05-13 12:07:03 +0300

    Change class Player: add methods setRowStep and setColStep, add Exceptions.

--- src/Player.java
+++ src/Player.java
@@ -13,12 +13,34 @@ public class Player {
     }
 
     void makeStep(Field gameField) {
+            if (!gameField.playerStep(setRowStep(), setColStep(), symbol)) {
+                makeStep(gameField);
+            }
+    }
+
+    private int setRowStep() {
         System.out.println("Введите номер ряда:");
-        int row = step.nextInt();
+        Scanner row = new Scanner(System.in);
+        try {
+            return row.nextInt();
+        }
+        catch(Exception ex) {
+            System.out.println("ОШИБКА! Введенные данные не соответствуют номеру ряда.");
+            System.out.println("Введите, пожалуйста, цифру от 1 до 3:");
+            return setRowStep();
+        }
+    }
+
+    private int setColStep() {
         System.out.println("Введите номер столбца:");
-        int col = step.nextInt();
-        if (!gameField.playerStep(row, col, symbol)) {
-            makeStep(gameField);
+        Scanner col = new Scanner(System.in);
+        try {
+            return col.nextInt();
+        }
+        catch(Exception ex) {
+            System.out.println("ОШИБКА! Введенные данные не соответствуют номеру столбца.");
+            System.out.println("Введите, пожалуйста, цифру от 1 до 3:");
+            return setColStep();
         }
     }
 }
