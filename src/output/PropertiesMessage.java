5f0aa45b044182bfb0ecbf536fc26d6106ece92d
sergei <sergeihrenovsky@yandex.by>
2020-05-19 09:20:54 +0300

    Change class PropertiesMessage

--- src/output/PropertiesMessage.java
+++ src/output/PropertiesMessage.java
@@ -4,10 +4,9 @@ import java.util.Properties;
 import java.io.*;
 
 public class PropertiesMessage {
-
-    private static FileInputStream fis;
+    private static InputStream fis;
     private static Properties property = new Properties();
-    private static String pathProperties = "src/resources/config.properties";
+    private static String pathProperties = "resources/config.properties";
 
     public static String getMessage(String keyProperty) {
         loadProperties();
@@ -15,19 +14,18 @@ public class PropertiesMessage {
             return  new String(property.getProperty(keyProperty).getBytes("ISO8859-1"));
         } catch (Exception ex) {
             ex.printStackTrace();
-            System.out.println(ex.getMessage());
             return "Error";
         }
     }
 
     private static void loadProperties() {
         try {
-            fis = new FileInputStream(pathProperties);
+            fis = PropertiesMessage.class.getClassLoader().getResourceAsStream(pathProperties);
             property.load(fis);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
